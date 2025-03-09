d947bc1514de799c6cec6dc6b27c4c148041d1f7
Payaldas <payaldaskunu30@gmail.com>
2025-03-09 22:41:38 +0530

    feat:ContactUs and footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import Contact from "./Components/contact";
 
 export default function Page() {
-    return <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    return <>
+    <h1 className="text-3xl font-bold underline">
+    </h1>
+  <Contact/>
+  </>
   }
