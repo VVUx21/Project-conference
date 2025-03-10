a6e67cb65f7b9f05b6da2543ba415cce1023574c
VVUNitian <bibhupb2004@gmail.com>
2025-03-11 02:17:51 +0530

    Add global styles, utility functions, and new components; update layout and dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import Hero from '../components/hero';
 
 export default function Page() {
-    return <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    return <div>
+      <Hero/>
+    </div>
   }
