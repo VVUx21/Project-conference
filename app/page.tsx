d6b1eca1d2a08225298bbb33d2446f4c85ce3c3d
Sanchita-nitr <sanchitapriyadarshinee@gmail.com>
2025-03-11 17:14:16 +0530

    final commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Contact from "./Components/contact";
 import Hero from '../components/hero';
+import AboutUs from "./Components/AboutUs";
+import Topics from "./Components/Topics";
 
 export default function Page() {
     return <div>
       <Hero/>
       <h1 className="text-3xl font-bold underline"></h1>
+      <AboutUs />
+      <Topics />
       <Contact/>
     </div>
   }
