d572e3a4d11849d5a972186704f895c7bcf09df1
VVUNitian <bibhupb2004@gmail.com>
2025-07-07 13:14:44 +0530

    feat: update Sponsors component with new styling and correct website links

--- components/Sponsors.tsx
+++ components/Sponsors.tsx
@@ -1,4 +1,3 @@
-// components/Sponsorship.tsx
 import Image from 'next/image'
 
 interface Sponsor {
@@ -10,33 +9,33 @@ interface Sponsor {
 
 const sponsors: Sponsor[] = [
   {
+    name: 'Sponsor Three',
+    logoSrc: 'https://res.cloudinary.com/dgtdkqfsx/image/upload/v1751311903/IMG-20250630-WA0005_yzwgjx.jpg',
+    website: '/#sponsorship',
+    altText: 'Sponsor Three Logo',
+  },
+  {
     name: 'Sponsor One',
-    logoSrc: 'https://res.cloudinary.com/dgtdkqfsx/image/upload/v1751311903/IMG-20250630-WA0002_sak5ct.jpg',
-    website: 'https://cvmi2025.nitrkl.ac.in/#sponsorship',
+    logoSrc: 'https://res.cloudinary.com/dgtdkqfsx/image/upload/v1751872914/WhatsApp_Image_2025-07-02_at_09.58.50_e152538e_ikibco.jpg',
+    website: '/#sponsorship',
     altText: 'Sponsor One Logo',
   },
   {
     name: 'Sponsor Two',
     logoSrc: 'https://res.cloudinary.com/dgtdkqfsx/image/upload/v1751311903/IMG-20250630-WA0004_h32omt.jpg',
-    website: 'https://cvmi2025.nitrkl.ac.in/#sponsorship',
+    website: '/#sponsorship',
     altText: 'Sponsor Two Logo',
-  },
-  {
-    name: 'Sponsor Three',
-    logoSrc: 'https://res.cloudinary.com/dgtdkqfsx/image/upload/v1751311903/IMG-20250630-WA0005_yzwgjx.jpg',
-    website: 'https://cvmi2025.nitrkl.ac.in/#sponsorship',
-    altText: 'Sponsor Three Logo',
   },
   {
     name: 'Sponsor Four',
     logoSrc: 'https://res.cloudinary.com/dgtdkqfsx/image/upload/v1751311903/IMG-20250630-WA0001_ddfwks.jpg',
-    website: 'https://cvmi2025.nitrkl.ac.in/#sponsorship',
+    website: '/#sponsorship',
     altText: 'Sponsor Four Logo',
   },
   {
     name: 'Sponsor Five',
     logoSrc: 'https://res.cloudinary.com/dgtdkqfsx/image/upload/v1751311903/IMG-20250630-WA0003_k249ha.jpg',
-    website: 'https://cvmi2025.nitrkl.ac.in/#sponsorship',
+    website: '/#sponsorship',
     altText: 'Sponsor Five Logo',
   },
 ]
@@ -48,30 +47,38 @@ const Sponsorship: React.FC = () => (
       Our Sponsors
             </h1>
       <div className="grid grid-cols-3 gap-6 items-center justify-items-center max-w-5xl mx-auto">
-        {sponsors.map((sponsor) => (
-          <a
-            key={sponsor.name}
-            href={sponsor.website}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="
-              block rounded-lg 
-              transform transition 
-              duration-300 ease-in-out 
-              hover:scale-105 hover:rotate-1 hover:shadow-lg hover:opacity-90
-            "
-          >
-            <Image
-              src={sponsor.logoSrc}
-              alt={sponsor.altText ?? sponsor.name}
-              width={160}
-              height={80}
-              className="object-contain mx-auto"
-            />
-          </a>
-
-        ))}
-      </div>
+      {sponsors.map((sponsor) => (
+        <a
+          key={sponsor.name}
+          href={sponsor.website}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="
+            rounded-xl p-8
+            bg-gradient-to-br from-orange-50 to-orange-100
+            border border-orange-200/50
+            shadow-md hover:shadow-xl
+            transform transition-all 
+            duration-300 ease-in-out 
+            hover:scale-105 hover:-translate-y-1
+            hover:bg-gradient-to-br hover:from-orange-100 hover:to-orange-150
+            group relative overflow-hidden
+            min-h-[140px] w-full
+            flex items-center justify-center
+          "
+        >
+          <div className="absolute inset-0 bg-gradient-to-br from-orange-500/5 to-orange-600/10 opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
+          
+          <Image
+            src={sponsor.logoSrc}
+            alt={sponsor.altText ?? sponsor.name}
+            width={160}
+            height={80}
+            className="object-contain mx-auto relative z-10 filter group-hover:brightness-110 transition-all duration-300"
+          />
+        </a>
+      ))}
+    </div>
     </div>
   </section>
 )
